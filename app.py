38cc267f0512e4a57477ecfc88bfde5e901d7ae2
tamco61 <42232243+tamco61@users.noreply.github.com>
2025-01-12 16:54:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pdfplumber
+from nn import create_sum
+
 
 def read_text_file(file):
     return file.read().decode("utf-8")
+
 
 def read_pdf_file(file):
     text = ""
@@ -11,18 +14,32 @@ def read_pdf_file(file):
             text += page.extract_text() + "\n"
     return text
 
+st.session_state.show_button = False
+
 st.title("Загрузка текстового или PDF файла")
 
 uploaded_file = st.file_uploader("Выберите файл", type=["txt", "pdf"])
-
+content = ""
+summary = ""
 if uploaded_file is not None:
+    p = False
     if uploaded_file.type == "text/plain":
-        # Если файл текстовый
+        p = True
         content = read_text_file(uploaded_file)
-        st.subheader("Содержимое текстового файла:")
+#        st.subheader("Содержимое текстового файла:")
+#        st.text(content)
     elif uploaded_file.type == "application/pdf":
-        # Если файл PDF
+        p = True
         content = read_pdf_file(uploaded_file)
-        st.subheader("Содержимое PDF файла:")
+#        st.subheader("Содержимое PDF файла:")
+#        st.text(content)
     else:
         st.error("Неподдерживаемый формат файла. Пожалуйста, загрузите текстовый или PDF файл.")
+    
+    if p:
+        if st.button("summarize"):
+            if summary == "":
+                summary = create_sum(content)
+            st.text(summary)
+
+
